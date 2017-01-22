90513645fafcb0f81bdbda86ee0639062a2ef1a0
David Hans <dahc36@gmail.com>
2017-01-22 20:31:50 -0300

    ADD: images

--- js/directory.js
+++ js/directory.js
@@ -1,13 +1,13 @@
 angular
-	.module('directoryApp', [])
+	.module('directoryApp', ['ngAnimate'])
 	.controller('directoryController', function(){
 		var dirList = this;
 
 		dirList.list = [
-			{name: 'Scott', age: 29},
-			{name: 'Ross', age: 31},
-			{name: 'Ben', age: 32},
-			{name: 'Courtney', age: 29}
+			{name: 'Scott', age: 29, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/ashleyford/128.jpg'},
+			{name: 'Ross', age: 31, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/marcosmoralez/128.jpg'},
+			{name: 'Ben', age: 32, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/abinav_t/128.jpg'},
+			{name: 'Courtney', age: 29, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg'}
 		];
 
 		dirList.addPerson = function(){
